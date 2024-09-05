794c574952d2a40eed6a024aed68bc9afc46d0c5
Kaua235 <kauaferreiraet2@gmail.com>
2024-09-04 21:10:03 -0300

    Enviando atividade-1

---
+++ atividade-1.py
@@ -0,0 +1,13 @@
+# Atividade 1 python
+# Inicializa as variáveis
+INDICE = 13
+SOMA = 0
+K = 0
+
+# Loop para somar os valores de 1 a INDICE
+while K < INDICE:
+    K += 1
+    SOMA += K
+
+# Exibe o resultado final
+print(f"O valor final da variável SOMA é {SOMA}")
